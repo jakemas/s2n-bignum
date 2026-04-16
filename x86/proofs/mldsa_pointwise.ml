ff576239f8c204e5285d6bff60378adc9276c991
Jake Massimo <jakemas@amazon.com>
2026-04-16 07:01:10 +0000

    Restore mldsa_pointwise.ml safety proofs lost during rebase

--- x86/proofs/mldsa_pointwise.ml
+++ x86/proofs/mldsa_pointwise.ml
@@ -174,55 +174,6 @@ let mldsa_pointwise_mc = define_assert_from_elf "mldsa_pointwise_mc" "x86/mldsa/
 let mldsa_pointwise_tmc = define_trimmed "mldsa_pointwise_tmc" mldsa_pointwise_mc;;
 let MLDSA_POINTWISE_TMC_EXEC = X86_MK_CORE_EXEC_RULE mldsa_pointwise_tmc;;
 
-(* ------------------------------------------------------------------------- *)
-(* Constants table: qinv (for Montgomery reduction) and Q (modulus)          *)
-(* Both broadcasted 8 times for SIMD processing                              *)
-(* ------------------------------------------------------------------------- *)
-
-let mldsa_pointwise_consts = define
- `mldsa_pointwise_consts:int list =
-   [&58728449; &58728449; &58728449; &58728449;
-    &58728449; &58728449; &58728449; &58728449;
-    &8380417; &8380417; &8380417; &8380417;
-    &8380417; &8380417; &8380417; &8380417]`;;
-
-(* ------------------------------------------------------------------------- *)
-(* Auxiliary lemmas                                                          *)
-(* ------------------------------------------------------------------------- *)
-
-(* ival of sign-extended product equals integer product when bounded by Q-1 *)
-let IVAL_WORD_MUL_SX32_64 = prove(
- `!x:int32 y:int32.
-    abs(ival x) <= &75423752 /\ abs(ival y) <= &75423752
-    ==> ival(word_mul (word_sx x:int64) (word_sx y:int64)) = ival x * ival y`,
-  REPEAT STRIP_TAC THEN
-  REWRITE_TAC[WORD_RULE `word_mul a b:int64 = iword(ival a * ival b)`] THEN
-  SIMP_TAC[IVAL_WORD_SX; DIMINDEX_32; DIMINDEX_64; ARITH] THEN
-  MATCH_MP_TAC IVAL_IWORD THEN REWRITE_TAC[DIMINDEX_64] THEN
-  CONV_TAC NUM_REDUCE_CONV THEN
-  SUBGOAL_THEN `abs(ival(x:int32) * ival(y:int32)) <= &5688742365757504` MP_TAC THENL
-   [REWRITE_TAC[INT_ABS_MUL] THEN
-    MATCH_MP_TAC INT_LE_TRANS THEN EXISTS_TAC `&75423752 * &75423752:int` THEN
-    CONJ_TAC THENL
-     [MATCH_MP_TAC INT_LE_MUL2 THEN ASM_REWRITE_TAC[INT_ABS_POS];
-      CONV_TAC INT_REDUCE_CONV];
-    REWRITE_TAC[INT_ABS_BOUNDS] THEN CONV_TAC INT_REDUCE_CONV THEN
-    INT_ARITH_TAC]);;
-
-let Q_MUL_COMM = WORD_RULE
- `word_mul (word 8380417:int64) x = word_mul x (word 8380417:int64)`;;
-
-(* Normalization rules for VPSRLQ/VMOVSHDUP patterns *)
-let USHR32_SUBWORD = WORD_BLAST
- `word_subword (word_ushr (x:int64) 32) (0,32):int32 = word_subword x (32,32)`;;
- 
-let DUP32_SUBWORD = WORD_BLAST
- `word_subword (word_duplicate (word_subword (x:int64) (32,32):int32):int64) (0,32):int32
-  = word_subword x (32,32)`;;
-
-(* Simplify word_subword(word_join ...) - needed for odd-indexed coefficients *)
-let WORD_JOIN_SUBWORD = WORD_BLAST
- `word_subword (word_join (a:int32) (b:int32):int64) (32,32):int32 = a`;;
 
 (* ========================================================================= *)
 (* Correctness proof                                                         *)
@@ -281,7 +232,7 @@ let MLDSA_POINTWISE_CORRECT = prove
   REPEAT STRIP_TAC THEN
   REWRITE_TAC [SOME_FLAGS; fst MLDSA_POINTWISE_TMC_EXEC] THEN
 
-  (* Phase 2: Ghost variables for YMM registers *)
+  (* Ghost variables for YMM registers *)
   GHOST_INTRO_TAC `init_ymm0:int256` `read YMM0` THEN
   GHOST_INTRO_TAC `init_ymm1:int256` `read YMM1` THEN
   GHOST_INTRO_TAC `init_ymm2:int256` `read YMM2` THEN
@@ -716,3 +667,234 @@ let MLDSA_POINTWISE_WINDOWS_SUBROUTINE_CORRECT = prove
   CONV_TAC TWEAK_CONV THEN
   MATCH_ACCEPT_TAC(ADD_IBT_RULE
     (CONV_RULE TWEAK_CONV MLDSA_POINTWISE_NOIBT_WINDOWS_SUBROUTINE_CORRECT)));;
+
+(* ========================================================================= *)
+(* Constant-time and memory safety proof.                                    *)
+(* ========================================================================= *)
+
+needs "x86/proofs/consttime.ml";;
+needs "x86/proofs/subroutine_signatures.ml";;
+
+let full_spec,public_vars = mk_safety_spec
+    ~keep_maychanges:true
+    (assoc "mldsa_pointwise_x86" subroutine_signatures)
+    MLDSA_POINTWISE_CORRECT
+    MLDSA_POINTWISE_TMC_EXEC;;
+
+let MLDSA_POINTWISE_SAFE =
+  REWRITE_RULE [MAYCHANGE_REGS_AND_FLAGS_PERMITTED_BY_ABI; SOME_FLAGS]
+  (time prove
+   (full_spec,
+    REWRITE_TAC[MAYCHANGE_REGS_AND_FLAGS_PERMITTED_BY_ABI; SOME_FLAGS] THEN
+    PROVE_SAFETY_SPEC_TAC ~public_vars:public_vars
+      MLDSA_POINTWISE_TMC_EXEC));;
+
+let MLDSA_POINTWISE_NOIBT_SUBROUTINE_SAFE = time prove
+ (`exists f_events.
+       forall e r a b consts pc stackpointer returnaddress.
+          aligned 32 r /\ aligned 32 a /\ aligned 32 b /\ aligned 32 consts /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_tmc) (r, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_tmc) (a, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_tmc) (b, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_tmc) (consts, 64) /\
+          nonoverlapping (r, 1024) (a, 1024) /\ nonoverlapping (r, 1024) (b, 1024) /\
+          nonoverlapping (r, 1024) (consts, 64) /\ nonoverlapping (a, 1024) (b, 1024) /\
+          nonoverlapping (a, 1024) (consts, 64) /\ nonoverlapping (b, 1024) (consts, 64) /\
+          nonoverlapping (stackpointer, 8) (r, 1024) /\
+          nonoverlapping (stackpointer, 8) (a, 1024) /\
+          nonoverlapping (stackpointer, 8) (b, 1024) /\
+          nonoverlapping (stackpointer, 8) (consts, 64)
+          ==> ensures x86
+               (\s. bytes_loaded s (word pc) mldsa_pointwise_tmc /\
+                    read RIP s = word pc /\ read RSP s = stackpointer /\
+                    read (memory :> bytes64 stackpointer) s = returnaddress /\
+                    C_ARGUMENTS [r; a; b; consts] s /\ read events s = e)
+               (\s. read RIP s = returnaddress /\
+                    read RSP s = word_add stackpointer (word 8) /\
+                    (exists e2. read events s = APPEND e2 e /\
+                         e2 = f_events r a b consts pc stackpointer returnaddress /\
+                         memaccess_inbounds e2
+                           [a,1024; b,1024; consts,64; r,1024; stackpointer,8]
+                           [r,1024; stackpointer,8]))
+               (\s s'. true)`,
+  X86_PROMOTE_RETURN_NOSTACK_TAC mldsa_pointwise_tmc
+    MLDSA_POINTWISE_SAFE THEN DISCHARGE_SAFETY_PROPERTY_TAC);;
+
+let MLDSA_POINTWISE_SUBROUTINE_SAFE = time prove
+ (`exists f_events.
+       forall e r a b consts pc stackpointer returnaddress.
+          aligned 32 r /\ aligned 32 a /\ aligned 32 b /\ aligned 32 consts /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_mc) (r, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_mc) (a, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_mc) (b, 1024) /\
+          nonoverlapping (word pc, LENGTH mldsa_pointwise_mc) (consts, 64) /\
+          nonoverlapping (r, 1024) (a, 1024) /\ nonoverlapping (r, 1024) (b, 1024) /\
+          nonoverlapping (r, 1024) (consts, 64) /\ nonoverlapping (a, 1024) (b, 1024) /\
+          nonoverlapping (a, 1024) (consts, 64) /\ nonoverlapping (b, 1024) (consts, 64) /\
+          nonoverlapping (stackpointer, 8) (r, 1024) /\
+          nonoverlapping (stackpointer, 8) (a, 1024) /\
+          nonoverlapping (stackpointer, 8) (b, 1024) /\
+          nonoverlapping (stackpointer, 8) (consts, 64)
+          ==> ensures x86
+               (\s. bytes_loaded s (word pc) mldsa_pointwise_mc /\
+                    read RIP s = word pc /\ read RSP s = stackpointer /\
+                    read (memory :> bytes64 stackpointer) s = returnaddress /\
+                    C_ARGUMENTS [r; a; b; consts] s /\ read events s = e)
+               (\s. read RIP s = returnaddress /\
+                    read RSP s = word_add stackpointer (word 8) /\
+                    (exists e2. read events s = APPEND e2 e /\
+                         e2 = f_events r a b consts pc stackpointer returnaddress /\
+                         memaccess_inbounds e2
+                           [a,1024; b,1024; consts,64; r,1024; stackpointer,8]
+                           [r,1024; stackpointer,8]))
+               (\s s'. true)`,
+  MATCH_ACCEPT_TAC(ADD_IBT_RULE MLDSA_POINTWISE_NOIBT_SUBROUTINE_SAFE));;
+
+(* ------------------------------------------------------------------------- *)
+(* Constant-time and memory safety proof of Windows ABI version.             *)
+(* ------------------------------------------------------------------------- *)
+
+let MLDSA_POINTWISE_NOIBT_WINDOWS_SUBROUTINE_SAFE = prove
+ (`exists f_events. forall e r a b consts pc stackpointer returnaddress.
+        aligned 32 r /\ aligned 32 a /\ aligned 32 b /\ aligned 32 consts /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_tmc) (r, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_tmc) (a, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_tmc) (b, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_tmc) (consts, 64) /\
+        nonoverlapping (r, 1024) (a, 1024) /\ nonoverlapping (r, 1024) (b, 1024) /\
+        nonoverlapping (r, 1024) (consts, 64) /\ nonoverlapping (a, 1024) (b, 1024) /\
+        nonoverlapping (a, 1024) (consts, 64) /\ nonoverlapping (b, 1024) (consts, 64) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (r, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (a, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (b, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (consts, 64) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_tmc)
+                       (word_sub stackpointer (word 192),192)
+        ==> ensures x86
+              (\s. bytes_loaded s (word pc) mldsa_pointwise_windows_tmc /\
+                  read RIP s = word pc /\ read RSP s = stackpointer /\
+                  read (memory :> bytes64 stackpointer) s = returnaddress /\
+                  WINDOWS_C_ARGUMENTS [r; a; b; consts] s /\ read events s = e)
+              (\s. read RIP s = returnaddress /\
+                  read RSP s = word_add stackpointer (word 8) /\
+                  (exists e2. read events s = APPEND e2 e /\
+                        e2 = f_events r a b consts pc
+                               (word_sub stackpointer (word 192)) returnaddress /\
+                        memaccess_inbounds e2
+                          [a,1024; b,1024; consts,64;
+                           r,1024; word_sub stackpointer (word 192),200]
+                          [r,1024; word_sub stackpointer (word 192),200]))
+              (MAYCHANGE [RSP] ,,
+               WINDOWS_MAYCHANGE_REGS_AND_FLAGS_PERMITTED_BY_ABI ,,
+               MAYCHANGE [memory :> bytes(word_sub stackpointer (word 192), 192)] ,,
+               MAYCHANGE [memory :> bytes(r, 1024)])`,
+
+  ASSUME_CALLEE_SAFETY_TAC MLDSA_POINTWISE_SAFE "H_subth" THEN
+  META_EXISTS_TAC THEN
+  REPLICATE_TAC 6 GEN_TAC THEN
+  WORD_FORALL_OFFSET_TAC 192 THEN REPEAT GEN_TAC THEN
+  REWRITE_TAC[fst MLDSA_POINTWISE_WINDOWS_TMC_EXEC] THEN
+  REPEAT STRIP_TAC THEN REWRITE_TAC[WINDOWS_C_ARGUMENTS] THEN
+  REWRITE_TAC[WINDOWS_MAYCHANGE_REGS_AND_FLAGS_PERMITTED_BY_ABI] THEN
+
+  ENSURES_PRESERVED_TAC "rdi_init" `RDI` THEN
+  ENSURES_PRESERVED_TAC "rsi_init" `RSI` THEN
+  ENSURES_PRESERVED_TAC "init_xmm6" `ZMM6 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm7" `ZMM7 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm8" `ZMM8 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm9" `ZMM9 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm10" `ZMM10 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm11" `ZMM11 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm12" `ZMM12 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm13" `ZMM13 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm14" `ZMM14 :> bottomhalf :> bottomhalf` THEN
+  ENSURES_PRESERVED_TAC "init_xmm15" `ZMM15 :> bottomhalf :> bottomhalf` THEN
+
+  REWRITE_TAC[READ_ZMM_BOTTOM_QUARTER'] THEN
+  REWRITE_TAC(map GSYM
+    [YMM6;YMM7;YMM8;YMM9;YMM10;YMM11;YMM12;YMM13;YMM14;YMM15]) THEN
+  GHOST_INTRO_TAC `init_ymm6:int256` `read YMM6` THEN
+  GHOST_INTRO_TAC `init_ymm7:int256` `read YMM7` THEN
+  GHOST_INTRO_TAC `init_ymm8:int256` `read YMM8` THEN
+  GHOST_INTRO_TAC `init_ymm9:int256` `read YMM9` THEN
+  GHOST_INTRO_TAC `init_ymm10:int256` `read YMM10` THEN
+  GHOST_INTRO_TAC `init_ymm11:int256` `read YMM11` THEN
+  GHOST_INTRO_TAC `init_ymm12:int256` `read YMM12` THEN
+  GHOST_INTRO_TAC `init_ymm13:int256` `read YMM13` THEN
+  GHOST_INTRO_TAC `init_ymm14:int256` `read YMM14` THEN
+  GHOST_INTRO_TAC `init_ymm15:int256` `read YMM15` THEN
+
+  GLOBALIZE_PRECONDITION_TAC THEN
+  REPEAT(FIRST_X_ASSUM(SUBST1_TAC o SYM)) THEN
+  ENSURES_INIT_TAC "s0" THEN
+  X86_STEPS_TAC MLDSA_POINTWISE_WINDOWS_TMC_EXEC (1--18) THEN
+  
+  W(fun (asl,w) ->
+    let current_events = List.filter_map (fun (_,ath) -> let t = concl ath in
+      if is_eq t && is_read_events (lhs t) then Some (rhs t)
+      else None) asl in
+    if length current_events <> 1
+    then failwith "More than 'read events .. = ..?'"
+    else
+      REMOVE_THEN "H_subth"
+        (MP_TAC o SPECL [hd current_events; `r:int64`; `a:int64`; `b:int64`;
+                         `consts:int64`; `pc + 106`]))
+  THEN
+  ASM_REWRITE_TAC[C_ARGUMENTS; SOME_FLAGS] THEN
+  ANTS_TAC THENL [NONOVERLAPPING_TAC; ALL_TAC] THEN
+  X86_BIGSTEP_TAC MLDSA_POINTWISE_WINDOWS_TMC_EXEC "s19" THENL
+   [FIRST_ASSUM(MATCH_ACCEPT_TAC o MATCH_MP
+     (BYTES_LOADED_SUBPROGRAM_RULE mldsa_pointwise_windows_tmc
+     (REWRITE_RULE[BUTLAST_CLAUSES]
+      (AP_TERM `BUTLAST:byte list->byte list` mldsa_pointwise_tmc))
+     106));
+    RULE_ASSUM_TAC(CONV_RULE(TRY_CONV RIP_PLUS_CONV))] THEN
+  MAP_EVERY ABBREV_TAC
+   [`ymm6_epilog = read YMM6 s19`; `ymm7_epilog = read YMM7 s19`;
+    `ymm8_epilog = read YMM8 s19`; `ymm9_epilog = read YMM9 s19`;
+    `ymm10_epilog = read YMM10 s19`; `ymm11_epilog = read YMM11 s19`;
+    `ymm12_epilog = read YMM12 s19`; `ymm13_epilog = read YMM13 s19`;
+    `ymm14_epilog = read YMM14 s19`; `ymm15_epilog = read YMM15 s19`] THEN
+  X86_STEPS_TAC MLDSA_POINTWISE_WINDOWS_TMC_EXEC (20--34) THEN
+  RULE_ASSUM_TAC(REWRITE_RULE[MAYCHANGE_ZMM_QUARTER]) THEN
+  RULE_ASSUM_TAC(REWRITE_RULE[MAYCHANGE_YMM_SSE_QUARTER]) THEN
+  ENSURES_FINAL_STATE_TAC THEN ASM_REWRITE_TAC[] THEN
+  CONJ_TAC THENL [ DISCHARGE_SAFETY_PROPERTY_TAC; ALL_TAC ] THEN
+  REPEAT CONJ_TAC THEN CONV_TAC WORD_BLAST);;
+
+let MLDSA_POINTWISE_WINDOWS_SUBROUTINE_SAFE = prove
+ (`exists f_events. forall e r a b consts pc stackpointer returnaddress.
+        aligned 32 r /\ aligned 32 a /\ aligned 32 b /\ aligned 32 consts /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_mc) (r, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_mc) (a, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_mc) (b, 1024) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_mc) (consts, 64) /\
+        nonoverlapping (r, 1024) (a, 1024) /\ nonoverlapping (r, 1024) (b, 1024) /\
+        nonoverlapping (r, 1024) (consts, 64) /\ nonoverlapping (a, 1024) (b, 1024) /\
+        nonoverlapping (a, 1024) (consts, 64) /\ nonoverlapping (b, 1024) (consts, 64) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (r, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (a, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (b, 1024) /\
+        nonoverlapping (word_sub stackpointer (word 192),200) (consts, 64) /\
+        nonoverlapping (word pc, LENGTH mldsa_pointwise_windows_mc)
+                       (word_sub stackpointer (word 192),192)
+        ==> ensures x86
+              (\s. bytes_loaded s (word pc) mldsa_pointwise_windows_mc /\
+                  read RIP s = word pc /\ read RSP s = stackpointer /\
+                  read (memory :> bytes64 stackpointer) s = returnaddress /\
+                  WINDOWS_C_ARGUMENTS [r; a; b; consts] s /\ read events s = e)
+              (\s. read RIP s = returnaddress /\
+                  read RSP s = word_add stackpointer (word 8) /\
+                  (exists e2. read events s = APPEND e2 e /\
+                        e2 = f_events r a b consts pc
+                               (word_sub stackpointer (word 192)) returnaddress /\
+                        memaccess_inbounds e2
+                          [a,1024; b,1024; consts,64;
+                           r,1024; word_sub stackpointer (word 192),200]
+                          [r,1024; word_sub stackpointer (word 192),200]))
+              (MAYCHANGE [RSP] ,,
+               WINDOWS_MAYCHANGE_REGS_AND_FLAGS_PERMITTED_BY_ABI ,,
+               MAYCHANGE [memory :> bytes(word_sub stackpointer (word 192), 192)] ,,
+               MAYCHANGE [memory :> bytes(r, 1024)])`,
+  MATCH_ACCEPT_TAC(ADD_IBT_RULE
+    MLDSA_POINTWISE_NOIBT_WINDOWS_SUBROUTINE_SAFE));;
