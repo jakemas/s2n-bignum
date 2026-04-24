48a65f7dd7d8800e597c9325bf74358802424471
Jake Massimo <jakemas@amazon.com>
2026-04-24 17:33:39 +0000

    Optimize Barrett equivalence proof with general interval lemma
    Replace the 1129-line case-split Barrett proof with a general
    DIV_SANDWICH + BARRETT_INTERVAL lemma approach. The general lemma
    proves that if both formulas are sandwiched in [k*d, (k+1)*d) for
    a given interval, then both equal k. The proof cascades through
    the 45-entry interval table, discharging numeric side conditions
    with NUM_REDUCE_CONV.
    
    Signed-off-by: Jake Massimo <jakemas@amazon.com>

--- arm/proofs/subroutine_signatures.ml
+++ arm/proofs/subroutine_signatures.ml
@@ -4418,19 +4418,20 @@ let subroutine_signatures = [
    ])
 );
 
-("mldsa_poly_use_hint_88",
+("mldsa_ntt_arm",
   ([(*args*)
-     ("b", "int32_t[static 256]", (*is const?*)"false");
-     ("a", "int32_t[static 256]", (*is const?*)"true");
-     ("h", "int32_t[static 256]", (*is const?*)"true");
+     ("a", "int32_t[static 256]", (*is const?*)"false");
+     ("z_012345", "int32_t[144]", (*is const?*)"true");
+     ("z_67", "int32_t[384]", (*is const?*)"true");
    ],
    "void",
    [(* input buffers *)
     ("a", "256"(* num elems *), 4(* elem bytesize *));
-    ("h", "256"(* num elems *), 4(* elem bytesize *));
+    ("z_012345", "144"(* num elems *), 4(* elem bytesize *));
+    ("z_67", "384"(* num elems *), 4(* elem bytesize *));
    ],
    [(* output buffers *)
-    ("b", "256"(* num elems *), 4(* elem bytesize *));
+    ("a", "256"(* num elems *), 4(* elem bytesize *));
    ],
    [(* temporary buffers *)
    ])
@@ -4449,6 +4450,24 @@ let subroutine_signatures = [
    ],
    [(* output buffers *)
     ("r", "256"(* num elems *), 4(* elem bytesize *));
+   ],
+   [(* temporary buffers *)
+   ])
+);
+
+("mldsa_poly_use_hint_88",
+  ([(*args*)
+     ("b", "int32_t[static 256]", (*is const?*)"false");
+     ("a", "int32_t[static 256]", (*is const?*)"true");
+     ("h", "int32_t[static 256]", (*is const?*)"true");
+   ],
+   "void",
+   [(* input buffers *)
+    ("a", "256"(* num elems *), 4(* elem bytesize *));
+    ("h", "256"(* num elems *), 4(* elem bytesize *));
+   ],
+   [(* output buffers *)
+    ("b", "256"(* num elems *), 4(* elem bytesize *));
    ],
    [(* temporary buffers *)
    ])
